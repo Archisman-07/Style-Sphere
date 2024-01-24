6cf8e8e904d704afba6d6cbf8817f0ed9fd26841
Archisman Roy <archisman2002@gmail.com>
2024-01-25 00:19:58 +0530

    minor changes

--- style-sphere/src/App.js
+++ style-sphere/src/App.js
@@ -1,8 +1,20 @@
-import Directory from './components/directory/directory.component.jsx';
+import {Routes,Route,Outlet} from 'react-router-dom';
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
+const Shop=()=>{
+  return <h1>I am the shop page</h1>
+};
 const App = ()=>{
   return (
-    <Directory/>
+    <Routes>
+      <Route path='/' element={<Navigation/>}>
+            <Route index element={<Home/>}/>
+            <Route path='shop' element={<Shop/>}/>
+            <Route path='sign-in' element={<SignIn/>}/>
+      </Route>
+    </Routes>
   );
 }
 
---
+++ style-sphere/src/routes/home/home.component.jsx
@@ -0,0 +1,9 @@
+import Directory from '../../components/directory/directory.component.jsx';
+
+const Home = ()=>{
+  return (
+    <Directory/>
+  );
+}
+
+export default Home;
